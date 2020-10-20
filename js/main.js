2b5855a15a789abf9e2a362f6ea6e7c41336097f
Ruslan <ruslanlozinskiy0@gmail.com>
2020-10-20 15:58:16 +0300

    edit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let counterMiniS = document.getElementById("counterMiniS");
+let counterS = document.getElementById("counterS");
+let start = document.getElementById('start');
+let reset = document.getElementById('reset');
+let pause = document.getElementById('pause');
