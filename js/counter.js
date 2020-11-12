bc6c3a0cf826c1eaa465deb3be9ea78ecd490cf2
SGvlack <ruslanlozinskiy0@gmail.com>
2020-11-12 17:36:43 +0200

    edit style

--- js/counter.js
+++ js/counter.js
@@ -7,13 +7,13 @@ function start() {
     } else {
          n++;
        getNumber.innerHTML = n;
-        get_rand_color();
+        getRandomColor();
     }
        
        
    
 }
-function get_rand_color() {
+function getRandomColor() {
     let color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
     while(color.length < 6) {
         color = "0" + color;
