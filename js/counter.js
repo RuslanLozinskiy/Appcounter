f76e61186bc661ed47737bf4d7db0d3542eedceb
SGvlack <ruslanlozinskiy0@gmail.com>
2020-11-23 20:09:49 +0200

    edit

--- js/counter.js
+++ js/counter.js
@@ -15,8 +15,8 @@ function start() {
 }
 function getRandomColor() {
     let color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
-    while(color.length < 6) {
-        color = "0" + color;
-    }
+//    while(color.length < 6) {
+//        color = "0" + color;
+//    }
     return getColor.style.backgroundColor =  color;
 }
